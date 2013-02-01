da44b62c437ef15d5e1f19115057ea5db5741029
Diego Cerdan <diegocerdan@gmail.com>
2013-02-01 12:46:29 +0100

    Cleaned the Q3 solution.

--- 03_find_the_min.py
+++ 03_find_the_min.py
@@ -6,34 +6,26 @@ import heapq
 import sys
 
 file = open(str(sys.argv[1]), 'r')
-lines = file.readlines()
 
-for j in range(1, len(lines), 2):
-    n, k = [int(x) for x in lines[j].split()]
-    a, b, c, r = [int(x) for x in lines[j+1].split()]
+cases = int(file.readline())
+
+for case in range(1, cases + 1):
+    n, k = [int(x) for x in file.readline().split()]
+    a, b, c, r = [int(x) for x in file.readline().split()]
+
+    n = k + (n+1) % (k+1)
 
     m = deque([a])
     for i in range(1, k):
         m.append((b * m[-1] + c) % r)
 
-    used = [False] * (k+1)
-    for i in m:
-        if i < k:
-            used[i] = True
-
-    non_used = []
-    for i, value in enumerate(used):
-        if not used[i]:
-            non_used.append(i)
-    heapq.heapify(non_used)
-
-
     counter = Counter(m)
 
-    final = ((n+1)%(k+1))
+    missing = [e for e in range(k+1) if not counter[e]]
+    heapq.heapify(missing)
 
-    for i in range(final):
-        min = heapq.heappop(non_used)
+    for i in range(k, n):
+        min = heapq.heappop(missing)
 
         m.append(min)
         counter[min] += 1
@@ -41,7 +33,7 @@ for j in range(1, len(lines), 2):
         discarded = m.popleft()
         counter[discarded] -= 1
 
-        if discarded < k and counter[discarded] == 0:
-            heapq.heappush(non_used, discarded)
+        if counter[discarded] == 0:
+            heapq.heappush(missing, discarded)
 
-    print('Case #', (j//2) + 1, ': ', m[-1], sep='')
+    print('Case #', case, ': ', m[-1], sep='')
